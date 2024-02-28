c08763cf7b78d762094e8b06e7a2a9984b47e9f9
kushalsamota <kushalsamota@gmail.com>
2024-02-28 14:51:48 +0530

    backend_removed

--- resource.tf
+++ resource.tf
@@ -2,12 +2,3 @@ resource "aws_instance" "webec2" {
   instance_type = "t2.micro"
   ami = "ami-0e670eb768a5fc3d4"
 }
-
-terraform {
-  backend "s3" {
-    bucket = "nomurabucket1"
-    key    = "terraform.tfstate"
-    region = "ap-south-1"
-    dynamodb_table = "s3-state-lock1"
-  }
-}
