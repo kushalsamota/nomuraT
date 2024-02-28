2e68b1abad9e902d7cde8cc7e9699488f667f068
kushalsamota <kushalsamota@gmail.com>
2024-02-28 12:06:54 +0530

    backend_configuration_Added

--- resource.tf
+++ resource.tf
@@ -2,3 +2,11 @@ resource "aws_instance" "webec2" {
   instance_type = "t2.micro"
   ami = "ami-0e670eb768a5fc3d4"
 }
+
+terraform {
+  backend "s3" {
+    bucket = "nomurabucket1"
+    key    = "terraform.tfstate"
+    region = "ap-south-1"
+  }
+}
